0bb106e475e12662866361abc94ca17e5e8d9c0e
Angrypig123 <shguddnr3@naver.com>
2023-08-03 12:44:48 +0900

    API 메시지 바디 -ãJSON 끝

---
+++ src/main/java/hello/servlet/basic/dto/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloData {
+    private String username;
+    private String age;
+}
--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -1,8 +1,8 @@
 package hello.servlet.basic.request;
 
-import org.apache.tomcat.util.buf.Utf8Encoder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.dto.HelloData;
 import org.springframework.util.StreamUtils;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
@@ -20,12 +19,22 @@ public class RequestBodyStringServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        ObjectMapper objectMapper = new ObjectMapper();
         ServletInputStream inputStream = req.getInputStream();  //  message body 의 내용을 byte 코드로 받는다.
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-        System.out.println(messageBody);
 
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().write(messageBody);
+        HelloData helloData;
+        try {
+            //  ToDO Post 요청을 보내는 것도 확인해보자! x-www-form-urlencoded 로 post 요청
+            helloData = objectMapper.readValue(messageBody, HelloData.class);
+            resp.setCharacterEncoding("utf-8");
+            resp.getWriter().write(helloData.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("변환 실패");
+        }
 
     }
+
 }
