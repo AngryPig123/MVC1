9a7935c8a3a69cce8a52ff642c2db178253cf1f3
Angrypig123 <shguddnr3@naver.com>
2023-08-03 18:56:34 +0900

    HttpServletResponse 기본 사용법 끝

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -38,3 +38,4 @@ public class RequestBodyStringServlet extends HttpServlet {
     }
 
 }
+
---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,53 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseHeader", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+//        resp.setHeader("Content-type", "text/plain;charset=utf-8;");
+        this.content(resp);
+        this.cookie(resp);
+        resp.setHeader("Cache-Control", "no-cache, no-store, no-revalidate");
+
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("myHeader", "hi");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        resp.getHeaderNames().iterator().forEachRemaining(item -> {
+            stringBuilder.append(item).append(" : ").append(resp.getHeader(item)).append("\n");
+        });
+
+        this.redirect(resp);
+//        resp.getWriter().write(stringBuilder.toString());
+
+    }
+
+    private void content(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+    }
+
+    private void cookie(HttpServletResponse resp) {
+        Cookie cookie = new Cookie("myCookie", "myCookieValue");
+        cookie.setDomain("localhost");
+        cookie.setMaxAge(500);
+        resp.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("/basic/hello-form.html");
+    }
+
+}
+
